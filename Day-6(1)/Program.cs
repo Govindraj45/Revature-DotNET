0cc91a5c2c306f7df541096a7ea8290199eca9e3
Govindraj Dapkekar <govindraj@gmail.com>
2026-02-10 12:28:09 +0530

    Update Program.cs with ref keyword example

---
+++ Day-6(1)/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegatesDemoApp app = new DelegatesDemoApp();
+            app.Run();
+        }
+    }
+}
